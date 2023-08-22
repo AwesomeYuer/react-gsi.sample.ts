fa9e6b38bb85e01ef0fb09ee368d76fb2b317b39
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-08-22 09:53:29 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import jwt_decode from 'jwt-decode';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  GsiButton,
+  GsiClient,
+  IdTokenProvider,
+  useIdToken
+} from 'react-gsi';
+
+
+import {
+  ErrorFallback,
+  IdleFallback,
+  LoadingFallback
+} from './Fallback';
+
+
+
+const idConfiguration: IdConfiguration = {
+  client_id: '944301045116-usq3bf0h2algmn9g39gp34qobs82171v.apps.googleusercontent.com',
+  auto_select: true // automatically sign in, see: https://developers.google.com/identity/gsi/web/guides/automatic-sign-in-sign-out
 }
+
+const buttonConfiguration: GsiButtonConfiguration = {
+  type: 'standard',
+  theme: 'outline',
+  size: 'large'
+}
+
+const signOut = () => {
+  // refresh the page
+  window.location.reload();
+}
+
+
+export function App() {
+  return (
+      <GsiClient idle={IdleFallback} loading={LoadingFallback} error={ErrorFallback}>
+          <IdTokenProvider configuration={idConfiguration}>
+              <Page />
+          </IdTokenProvider>
+      </GsiClient>
+  )
+}
+
+function Page() {
+  const token = useIdToken();
+
+  
+
+
+  const signedOut = token === null;
+
+  // useOneTap({
+  //     show: signedOut
+  // })
+
+  if (signedOut) {
+      return (
+          <>
+              <h1>Logged Out</h1>
+              <GsiButton configuration={buttonConfiguration} />
+          </>
+      );
+  } else {
+      const { select_by, credential } = token;
+
+      const jwt = jwt_decode(credential);
+
+      return (
+          <>
+              <h1>Logged In via {select_by}</h1>
+              <p>{credential}</p>
+              <p>
+              <pre style={{ maxHeight: '400px', overflow: 'auto', padding: '0.5em 0' }}>
+                {JSON.stringify(jwt, null, 2)}
+              </pre>
+              </p>
+              <button className="g_id_signout" onClick={() => signOut()}>Sign Out</button>
+          </>
+      )
+  }
+}
+
+
 
 export default App;
---
+++ src/Fallback.tsx
@@ -0,0 +1,26 @@
+import {
+    ScriptErrorStatus,
+    ScriptFallback,
+    ScriptIdleStatus,
+    ScriptLoadingStatus
+} from 'react-gsi';
+
+export const IdleFallback: ScriptFallback<ScriptIdleStatus> = () => {
+    return <strong>Waiting For Library&hellip;</strong>;
+};
+
+export const LoadingFallback: ScriptFallback<ScriptLoadingStatus> = () => {
+    return <strong>Loading Library&hellip;</strong>;
+};
+
+export const ErrorFallback: ScriptFallback<ScriptErrorStatus> = (status) => {
+    const { event } = status;
+
+    return (
+        <div style={{ width: 600 }}>
+            <strong>Error Loading Library</strong>
+            <p>The following error occurred:</p>
+            <pre><code>{JSON.stringify(event)}</code></pre>
+        </div>
+    );
+};
